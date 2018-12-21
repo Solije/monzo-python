08ccbd0333eaa95298270df57785bc9430099ebd
Tom French <tom@tomfren.ch>
2018-12-21 19:11:06 +0000

    Import classes into __init__.py

--- monzo/__init__.py
+++ monzo/__init__.py
@@ -1,1 +1,6 @@
 
+from monzo.monzo import Monzo
+from monzo.auth import MonzoOAuth2Client
+
+from monzo.account import Account, Pot
+from monzo.transaction import Transaction, TransactionList, Merchant
